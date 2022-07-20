22c6e36c99b2a78715b6c73f23193cf1f78ce368
Olavo Candido <olavomc@gmail.com>
2022-07-20 11:10:16 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CursoEAD
+ Aprendendo HTLM / CSS / C#
---
+++ UC3/DesafioC#/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace DesafioC_{
+    class Program{
+        static void Main(string[] args){
+            
+            //Console.WriteLine("Digite a base do triangulo: ");
+            //float alt = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Digite a altura do triangulo: ");
+            //float altura = float.Parse(Console.ReadLine());
+            //float area = (alt * altura) / 2;
+            //Console.WriteLine("A area é: "+ area);
+            
+            //Console.WriteLine("Informe quantos sua idade: ");
+            //int idade = int.Parse(Console.ReadLine());
+            //int dias = idade * 365;
+            //Console.WriteLine("Seus dias são: "+ dias);
+
+            //Console.WriteLine("Informe um numero: ");
+            //int num = int.Parse(Console.ReadLine());
+            //int ant = num - 1;
+            //int suc = num + 1;
+            //Console.WriteLine("Antecessor é: {0} Sucessor é: {1}", ant, suc);
+
+            //Console.WriteLine("Informe o raio: ");
+            //double raio = double.Parse(Console.ReadLine());
+            //double valor = (Math.Pow(raio, 2)) * 3.14;
+            //Console.WriteLine("A area do circulo é: "+ valor); 
+
+            //Console.WriteLine("Informe quantos KM vaiser feito: ");
+            //float km = float.Parse(Console.ReadLine());
+            //float litro = km / 12;
+            //Console.WriteLine("São necessario: {0}L",litro);
+
+            //Console.WriteLine("Informe a altura: ");
+            //double altura = double.Parse(Console.ReadLine());
+            //Console.WriteLine(altura);
+            //if(altura >= 1.80){
+            //    Console.WriteLine("Alto");
+            //}
+            //else{
+            //    Console.WriteLine("Baixo");
+            //}
+
+            //Console.WriteLine("Informe um valor: ");
+            //int num1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Informe outro valor valor: ");
+            //int num2 = int.Parse(Console.ReadLine());
+            //if(num1 < num2){
+            //    Console.WriteLine("Ordem é {0}, {1}", num1, num2);
+            //}
+            //else{
+            //    Console.WriteLine("Ordem é {0}, {1}", num2, num1);
+            //}
+
+            //Console.WriteLine("Informe o nome do primeiro time: ");
+            //string time1 = Console.ReadLine();
+            //Console.WriteLine("Informe a quantidade de gols: ");
+            //int gol1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Informe o nome do segundo time: ");
+            //string time2 = Console.ReadLine();
+            //Console.WriteLine("Informe a quantidade de gols: ");
+            //int gol2 = int.Parse(Console.ReadLine());
+            //if(gol1 > gol2){
+            //    Console.WriteLine("Vencedor é: {0}", time1);
+            //}
+            //if(gol2 > gol1){
+            //    Console.WriteLine("Vencedor é: {0}", time2);
+            //}
+            //if(gol1 == gol2){
+            //    Console.WriteLine("Empate");
+            //}
+
+
+        }
+    }
+}
